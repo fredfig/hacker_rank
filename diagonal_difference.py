3b4f3dee57e58c19c2ba3892e8567bf078027909
fredfig <fredcomp@gmail.com>
2021-11-30 10:55:21 -0300

    Diagonal Difference challenge - Final

--- diagonal_difference.py
+++ diagonal_difference.py
@@ -14,7 +14,20 @@ import sys
 #
 
 def diagonalDifference(arr):
-    # Write your code here
+
+    # Declaration of the diagonals and the size of the matrix
+    left_diagonal = 0
+    right_diagonal = 0
+    n = len(arr)
+
+    # Iterate on the matrix
+    for i in range(0, n):
+
+        left_diagonal += arr[i][i]
+        right_diagonal += arr[i][n - i - 1]
+
+    # Return of the absolute value of the diagonal difference sum
+    return abs(left_diagonal - right_diagonal)
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
