05da1788c7dbc68bc2fee20a0a41bb737c22cbee
fredfig <fredcomp@gmail.com>
2021-11-30 17:03:42 -0300

    Counting Sort 1 challenge

---
+++ counting_sort_1.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    size_array = len(arr)
+    frequency_array = [0] * 100
+    x = 0
+
+    for x in range(size_array):
+        frequency_array[arr[x]] += 1
+
+    return frequency_array
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
