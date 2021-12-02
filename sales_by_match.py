43f073a2bc1c788e95e4bd8e1265f72c42a5361f
fredfig <fredcomp@gmail.com>
2021-12-02 14:35:31 -0300

    Sales by Match challenge

---
+++ sales_by_match.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    ar.sort()
+    num_pairs = 0
+    x = 0
+    while x < n-1:
+        if ar[x] == ar[x+1]:
+            num_pairs += 1
+            x += 2
+            continue
+        x += 1
+    return num_pairs
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
